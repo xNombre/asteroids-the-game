9f54ae161a33a68121394148bd273d41dcd7658f
Andrzej Perczak <linux@andrzejperczak.com>
2023-04-27 21:30:18 +0200

    Add fluent ship moving mechanism

--- main.cpp
+++ main.cpp
@@ -1,123 +1,156 @@
-#include <GLUT/glut.h>
 
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <iostream>
+
+std::mutex ship_pos_mtx;
 float x_position = 0.0f;
 float y_position = -0.8f;
 
+float triangle_speed = 0.005f;
 
-float triangle_speed = 0.5f; // pixels per second
-
-int window_width = 640;
-int window_height = 480;
+int window_width = 800;
+int window_height = 600;
 
 void display()
 {
-    float scale = 10.0f;
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-     glTranslatef(0.0f, 0.0f, 0.0f);
-     glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glVertex2f(0.0f/scale, 1.0f/scale);
-    glVertex2f(-0.6f/scale, -0.6f/scale);
-    glVertex2f(0.0f/scale,  -0.3f/scale);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(0.0f/scale, 1.0f/scale);
-    glVertex2f(0.6f/scale, -0.6f/scale);
-    glVertex2f(0.0f/scale,  -0.3f/scale);
-    glEnd();
+	float scale = 10.0f;
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, 0.0f);
 
+	{
+		std::lock_guard<std::mutex> lock(ship_pos_mtx);
+		auto x_position_swp = x_position;
+	}
+	glTranslatef(x_position, y_position, 0.0f);
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.0f / scale, 1.0f / scale);
+	glVertex2f(-0.6f / scale, -0.6f / scale);
+	glVertex2f(0.0f / scale, -0.3f / scale);
+	glBegin(GL_TRIANGLES);
+	glVertex2f(0.0f / scale, 1.0f / scale);
+	glVertex2f(0.6f / scale, -0.6f / scale);
+	glVertex2f(0.0f / scale, -0.3f / scale);
+	glEnd();
 
-    glTranslatef(x_position, y_position, 0.0f);
-    
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glVertex2f(0.0f/scale, 1.0f/scale);
-    glVertex2f(-0.6f/scale, -0.6f/scale);
-    glVertex2f(0.0f/scale,  -0.3f/scale);
-    glBegin(GL_TRIANGLES);
-    glVertex2f(0.0f/scale, 1.0f/scale);
-    glVertex2f(0.6f/scale, -0.6f/scale);
-    glVertex2f(0.0f/scale,  -0.3f/scale);
+	glFlush();
+	glutPostRedisplay();
 
-
-    glEnd();
-   
-    glFlush();
-    glutPostRedisplay();
-
-    glutSwapBuffers();
+	glutSwapBuffers();
 }
 
 void resize(int w, int h)
 {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+	glutReshapeWindow(window_width, window_height);
 }
 
+void move_thread();
+enum class move_dir_enum {
+	NONE,
+	LEFT,
+	RIGHT
+};
+move_dir_enum ship_move_dir = move_dir_enum::NONE;
+std::thread ship_move_thread;
+std::condition_variable move_dir_cond;
 
-void update(int elapsed_time) {
-    float delta_time = elapsed_time / 1000.0f; // convert to seconds
-    
-    float delta_position_x = triangle_speed * delta_time;
-    
-    
-    // wrap triangle position around the screen
-    if (x_position > window_width / 2.0f) {
-        x_position = -window_width / 2.0f;
-    }
-    
-    glutPostRedisplay();
-    
-    // call this function again after a delay
-    glutTimerFunc(16, update, glutGet(GLUT_ELAPSED_TIME));
-}
-
-
-void specialKeys(unsigned char key, int x, int y)
+void specialKeys(int key, int x, int y)
 {
-    switch (key)
-    {
-    case GLUT_KEY_LEFT:
-        x_position -= triangle_speed * 10.0f;
-        break;
-    case GLUT_KEY_RIGHT:
-        x_position += triangle_speed * 10.0f;
-        break;
-    }
+	move_dir_enum new_dir = move_dir_enum::NONE;
+	switch (key) {
+	case GLUT_KEY_LEFT: {
+		new_dir = move_dir_enum::LEFT;
+		break;
+	}
+	case GLUT_KEY_RIGHT:
+		new_dir = move_dir_enum::RIGHT;
+		break;
+	}
 
+	if (new_dir == move_dir_enum::NONE)
+		return;
+
+	std::unique_lock<std::mutex> lock(ship_pos_mtx);
+	if (ship_move_dir == new_dir)
+		return;
+	if (ship_move_dir == move_dir_enum::NONE) {
+		ship_move_thread = std::thread(move_thread);
+	}
+
+	ship_move_dir = new_dir;
 }
 
 
-void specialKeysUp(unsigned char key, int x, int y)
+void specialKeysUp(int key, int x, int y)
 {
-    switch (key)
-    {
-    case GLUT_KEY_LEFT:
-        x_position -= triangle_speed * 10.0f;
-        break;
-    case GLUT_KEY_RIGHT:
-        x_position += triangle_speed * 10.0f;
-        break;
-    }
+	move_dir_enum new_dir = move_dir_enum::NONE;
+	switch (key) {
+	case GLUT_KEY_LEFT: {
+		new_dir = move_dir_enum::LEFT;
+		break;
+	}
+	case GLUT_KEY_RIGHT:
+		new_dir = move_dir_enum::RIGHT;
+		break;
+	}
 
+	if (new_dir == move_dir_enum::NONE)
+		return;
+
+	std::unique_lock<std::mutex> lock(ship_pos_mtx);
+	if (ship_move_dir == new_dir) {
+		ship_move_dir = move_dir_enum::NONE;
+		move_dir_cond.notify_one();
+		lock.unlock();
+		ship_move_thread.join();
+	}
 }
 
+void move_thread()
+{
+	std::unique_lock<std::mutex> lock(ship_pos_mtx);
+	while (ship_move_dir != move_dir_enum::NONE) {
+
+		if (ship_move_dir == move_dir_enum::LEFT) {
+			x_position -= triangle_speed / 10.0f;
+			if (x_position < -1) {
+				x_position = -1;
+			}
+		}
+		else {
+			x_position += triangle_speed / 10.0f;
+			if (x_position > 1) {
+				x_position = 1;
+			}
+		}
+
+		move_dir_cond.wait_for(lock, std::chrono::microseconds(400));
+	}
+}
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(800, 600);
-    glutCreateWindow("Triangle");
-    glutDisplayFunc(display);
-    glutReshapeFunc(resize);
-    glutKeyboardFunc(specialKeys);
-    glutKeyboardUpFunc(specialKeysUp);
-    glutTimerFunc(0, update, glutGet(GLUT_ELAPSED_TIME));
-    glutMainLoop();
-    return 0;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(window_width, window_height);
+	glutCreateWindow("Asteroids - The Game");
+	glutDisplayFunc(display);
+	glutReshapeFunc(resize);
+	glutSpecialFunc(specialKeys);
+	glutSpecialUpFunc(specialKeysUp);
+	glutMainLoop();
+
+	return 0;
 }
