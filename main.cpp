33e85fd330dcba5d92dbee95b911e17dfd1502ff
Torasek <torasek@wp.pl>
2023-04-27 18:48:02 +0200

    Main update
    Modified movement

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <GLUT/glut.h>
 
-GLfloat x_position = 0.0f;
-GLfloat y_position = -0.8f;
+float x_position = 0.0f;
+float y_position = -0.8f;
+
+
+float triangle_speed = 0.5f; // pixels per second
+
+int window_width = 640;
+int window_height = 480;
 
 void display()
 {
@@ -22,7 +28,7 @@ void display()
     glVertex2f(0.0f/scale,  -0.3f/scale);
     glEnd();
 
-    
+
     glTranslatef(x_position, y_position, 0.0f);
     
     glBegin(GL_TRIANGLES);
@@ -53,21 +59,53 @@ void resize(int w, int h)
 }
 
 
-void specialKeys(int key, int x, int y)
+void update(int elapsed_time) {
+    float delta_time = elapsed_time / 1000.0f; // convert to seconds
+    
+    float delta_position_x = triangle_speed * delta_time;
+    
+    
+    // wrap triangle position around the screen
+    if (x_position > window_width / 2.0f) {
+        x_position = -window_width / 2.0f;
+    }
+    
+    glutPostRedisplay();
+    
+    // call this function again after a delay
+    glutTimerFunc(16, update, glutGet(GLUT_ELAPSED_TIME));
+}
+
+
+void specialKeys(unsigned char key, int x, int y)
 {
     switch (key)
     {
     case GLUT_KEY_LEFT:
-        x_position -= 0.05f;
-        glutPostRedisplay();
+        x_position -= triangle_speed * 10.0f;
         break;
     case GLUT_KEY_RIGHT:
-        x_position += 0.05f;
-        glutPostRedisplay();
+        x_position += triangle_speed * 10.0f;
         break;
     }
 
 }
+
+
+void specialKeysUp(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case GLUT_KEY_LEFT:
+        x_position -= triangle_speed * 10.0f;
+        break;
+    case GLUT_KEY_RIGHT:
+        x_position += triangle_speed * 10.0f;
+        break;
+    }
+
+}
+
 
 int main(int argc, char** argv)
 {
@@ -77,7 +115,9 @@ int main(int argc, char** argv)
     glutCreateWindow("Triangle");
     glutDisplayFunc(display);
     glutReshapeFunc(resize);
-    glutSpecialFunc(specialKeys);
+    glutKeyboardFunc(specialKeys);
+    glutKeyboardUpFunc(specialKeysUp);
+    glutTimerFunc(0, update, glutGet(GLUT_ELAPSED_TIME));
     glutMainLoop();
     return 0;
 }
